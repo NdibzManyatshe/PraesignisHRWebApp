38a0ed702a25715b9ff864dab85026c91ee165c2
NdibzManyatshe <Ndibz.manyatshe@praesignis.com>
2020-09-10 18:27:24 +0200

    Add project files.

---
+++ PraesignisHRWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PraesignisHRWebApp.MvcApplication" Language="C#" %>
---
+++ PraesignisHRWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PraesignisHRWebApp.Startup))]
+namespace PraesignisHRWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
